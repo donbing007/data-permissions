7e980c87d2b245df6e369099be4692539d639dbf
董斌 <dongbing@xforceplus.com>
2019-11-15 13:24:41 +0800

    fix check sylte error.

--- xplat-data-permissions-api/src/main/java/com/xforceplus/ultraman/permissions/api/SqlTestController.java
+++ xplat-data-permissions-api/src/main/java/com/xforceplus/ultraman/permissions/api/SqlTestController.java
@@ -17,8 +17,8 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.annotation.Resource;
 
 /**
- * @version 0.1 2019/11/14 15:32
  * @author dongbin
+ * @version 0.1 2019/11/14 15:32
  * @since 1.8
  */
 @Api(value = "SqlTestController")
@@ -28,21 +28,21 @@ public class SqlTestController {
     @Resource
     private RuleCheckService ruleCheckService;
 
-    @ApiOperation(value = "测试 SQL 的规则验证.", notes = "", response = CheckResult.class, tags = {"sql",})
+    @ApiOperation(value = "测试 SQL 的规则验证.", notes = "", response = CheckResult.class, tags = {"sql"})
     @ApiResponses(value = {
-        @ApiResponse(code = 200, message = "得到规则验证后的结果.", response = CheckResult.class)}
+            @ApiResponse(code = 200, message = "得到规则验证后的结果.", response = CheckResult.class)}
     )
     @GetMapping(value = "/{tenant}/{role}/v1/sql", produces = {MediaType.APPLICATION_JSON_VALUE})
     public ResponseEntity test(
-        @ApiParam(name = "sql", value = "原始 sql", required = true) @RequestParam("sql") String sql,
-        @ApiParam(name = "tenant", value = "发起 SQL 的当前租户.", required = true) @PathVariable("tenant") String tenant,
-        @ApiParam(name = "role", value = "发起 SQL 的当前租户角色.", required = true) @PathVariable("role") String role) {
+            @ApiParam(name = "sql", value = "原始 sql", required = true) @RequestParam("sql") String sql,
+            @ApiParam(name = "tenant", value = "发起 SQL 的当前租户.", required = true) @PathVariable("tenant") String tenant,
+            @ApiParam(name = "role", value = "发起 SQL 的当前租户角色.", required = true) @PathVariable("role") String role) {
 
         CheckResult result = ruleCheckService.check(sql, new Authorization(role, tenant));
 
         return new ResponseEntity(
-            result,
-            HttpCodeAssembler.assem(CheckStatus.getInstance(result.getCode()), HttpStatus.OK));
+                result,
+                HttpCodeAssembler.assem(CheckStatus.getInstance(result.getCode()), HttpStatus.OK));
 
     }
 }
