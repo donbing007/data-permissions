5a8b66f9aa855c357c4723fdd8c2d71e079ec077
董斌 <dongbing@xforceplus.com>
2020-06-18 16:16:52 +0800

    fix no role

--- xplat-data-permissions-jdbc/src/main/java/com/xforceplus/ultraman/permissions/jdbc/authorization/impl/ContextAuthorizationSearcher.java
+++ xplat-data-permissions-jdbc/src/main/java/com/xforceplus/ultraman/permissions/jdbc/authorization/impl/ContextAuthorizationSearcher.java
@@ -1,5 +1,10 @@
 package com.xforceplus.ultraman.permissions.jdbc.authorization.impl;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 import com.xforceplus.tenantsecurity.domain.IAuthorizedUser;
 import com.xforceplus.tenantsecurity.domain.IRole;
 import com.xforceplus.tenantsecurity.domain.UserInfoHolder;
@@ -7,34 +12,35 @@ import com.xforceplus.ultraman.permissions.jdbc.authorization.AuthorizationSearc
 import com.xforceplus.ultraman.permissions.pojo.auth.Authorization;
 import com.xforceplus.ultraman.permissions.pojo.auth.Authorizations;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
-
 /**
- * 依赖用户中心的授权信息上下文传递.
- * 从线程上下文中拿取授权信息.
+ * 依赖用户中心的授权信息上下文传递. 从线程上下文中拿取授权信息.
+ * 
  * @version 0.1 2019/11/19 15:38
  * @author dongbin
  * @since 1.8
  */
 public class ContextAuthorizationSearcher implements AuthorizationSearcher {
 
-    @Override
-    public Authorizations search() {
+  @Override
+  public Authorizations search() {
 
-        IAuthorizedUser authorizedUser = UserInfoHolder.get();
-        Set<IRole> roles = authorizedUser.getRoles();
-
-        List<Authorization> authorizations;
-        if (roles != null) {
-            authorizations = roles.stream().map(r ->
-                new Authorization(Long.toString(r.getId()), Long.toString(r.getTenantId()))).collect(Collectors.toList());
-        } else {
-            authorizations = Collections.emptyList();
-        }
-
-        return new Authorizations(authorizations);
+    IAuthorizedUser authorizedUser = UserInfoHolder.get();
+    Set<IRole> roles = null;
+    if (authorizedUser == null) {
+      roles = Collections.emptySet();
+    } else {
+      roles = authorizedUser.getRoles();
     }
+
+    List<Authorization> authorizations;
+    if (roles != null) {
+      authorizations = roles.stream()
+          .map(r -> new Authorization(Long.toString(r.getId()), Long.toString(r.getTenantId())))
+          .collect(Collectors.toList());
+    } else {
+      authorizations = Collections.emptyList();
+    }
+
+    return new Authorizations(authorizations);
+  }
 }
