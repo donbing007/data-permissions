f54dc8f12d5a754ac96080652f24be57ff48a06f
董斌 <dongbing@xforceplus.com>
2020-06-30 14:30:47 +0800

    1. 修正用户中心依赖为2.x

--- xplat-data-permissions-jdbc/src/main/java/com/xforceplus/ultraman/permissions/jdbc/authorization/impl/ContextAuthorizationSearcher.java
+++ xplat-data-permissions-jdbc/src/main/java/com/xforceplus/ultraman/permissions/jdbc/authorization/impl/ContextAuthorizationSearcher.java
@@ -1,22 +1,22 @@
 package com.xforceplus.ultraman.permissions.jdbc.authorization.impl;
 
+import com.xforceplus.tenant.security.core.context.UserInfoHolder;
+import com.xforceplus.tenant.security.core.domain.IAuthorizedUser;
+import com.xforceplus.tenant.security.token.domain.IRole;
+import com.xforceplus.ultraman.permissions.jdbc.authorization.AuthorizationSearcher;
+import com.xforceplus.ultraman.permissions.pojo.auth.Authorization;
+import com.xforceplus.ultraman.permissions.pojo.auth.Authorizations;
+
 import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-import com.xforceplus.tenantsecurity.domain.IAuthorizedUser;
-import com.xforceplus.tenantsecurity.domain.IRole;
-import com.xforceplus.tenantsecurity.domain.UserInfoHolder;
-import com.xforceplus.ultraman.permissions.jdbc.authorization.AuthorizationSearcher;
-import com.xforceplus.ultraman.permissions.pojo.auth.Authorization;
-import com.xforceplus.ultraman.permissions.pojo.auth.Authorizations;
-
 /**
  * 依赖用户中心的授权信息上下文传递. 从线程上下文中拿取授权信息.
- * 
- * @version 0.1 2019/11/19 15:38
+ *
  * @author dongbin
+ * @version 0.1 2019/11/19 15:38
  * @since 1.8
  */
 public class ContextAuthorizationSearcher implements AuthorizationSearcher {
